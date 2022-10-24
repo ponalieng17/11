372db8957da6fc9e8a04a777081af6531ed400bf
ponalieng17 <106666400+ponalieng17@users.noreply.github.com>
2022-10-24 16:59:10 +0300

    Create from_26.02.2022.cpp

---
+++ from_26.02.2022.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	const int MAXLEN = 256;
+	char string[MAXLEN];
+	char string_1[MAXLEN];
+	
+	cout << "Введите строку: ";
+	gets_s(string);
+	cout << "Ваша строка: " << string << endl;
+	cout << "Занято элементов: " << strlen(string) << endl;
+	cout << "Свободных элементов: " << MAXLEN - strlen(string) << endl << endl;
+
+	int m, n;
+	cout << "Введите номер символа строки, с которого будет выводится строка: ";
+	cin >> m;
+	cout << "Введите номер символа строки, до которого будет выводится строка: ";
+	cin >> n;
+	cout << "Новый отрезок с 'm по n': ";
+	int i, j;
+        for (i = m - 1; i < n; i++)
+	if ((m<0 || m>strlen(string)) && (n<0 || n>strlen(string)))
+		cout << "Вы вышли за пределы фиксированного массива строк!";
+	else
+		cout << string[i];
+		
+			
+
+		
+	
+	cout << endl << endl;
+	
+	
+	
+}
+
